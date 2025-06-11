777ec956a58473e6b318659c51f199c016cada41
winstoncda <olivier.pro.cda@gmail.com>
2025-06-11 13:28:56 +0200

    mise en place architecture et routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <>
-      <h1>Tailwind Version 4 Model</h1>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
+      <NavBar />
+      <Outlet />
     </>
   );
 }
--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,36 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/signup",
+        element: <SignUpPage />,
+      },
+      {
+        path: "/profile",
+        element: <ProfilePage />,
+      },
+      {
+        path: "/settings",
+        element: <SettingsPage />,
+      },
+    ],
   },
 ]);
