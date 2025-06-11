3b539ac65ecaa13395acc57e6b4d390c6e571d12
winstoncda <olivier.pro.cda@gmail.com>
2025-06-11 13:51:21 +0200

    mise en place axios et store pour l'authentification

---
+++ src/store/useAuthStore.js
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in check auth", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
